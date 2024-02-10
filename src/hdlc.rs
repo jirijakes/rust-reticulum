eb5fd2bda6fc23faf9eb87bfdf37c41b7d8e21f8
jiri <>
2024-02-10 13:27:25 +0000

    Add HRLC Write
    FossilOrigin-Name: 5b5bd95a7965bab9d32e4fb5785b6b573a9143af633104b71354207266a9de32

--- src/hdlc.rs
+++ src/hdlc.rs
@@ -1,24 +1,44 @@
-use std::io::{Read, Result};
+use std::io::{Read, Result, Write};
 
 const FRAME_BOUNDARY: u8 = 0x7e;
 const ESCAPE_BYTE: u8 = 0x7d;
 const FLIP_MASK: u8 = 0b00100000;
 
-pub struct Hdlc<R> {
-    inner: R,
+pub struct Hdlc<I> {
+    inner: I,
     escaping: bool,
     started: bool,
     finished: bool,
 }
 
-impl<R: Read> Hdlc<R> {
-    pub const fn new(inner: R) -> Self {
+impl<I> Hdlc<I> {
+    pub const fn new(inner: I) -> Self {
         Self {
             inner,
             escaping: false,
             started: false,
             finished: false,
         }
+    }
+}
+
+impl<W: Write> Write for Hdlc<W> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        let _ = self.inner.write(&[FRAME_BOUNDARY])?;
+
+        for b in buf {
+            if *b == FRAME_BOUNDARY || *b == ESCAPE_BYTE {
+		let _ = self.inner.write(&[ESCAPE_BYTE, *b ^ FLIP_MASK])?;
+            } else {
+                let _ = self.inner.write(&[*b])?;
+            }
+        }
+        let _ = self.inner.write(&[FRAME_BOUNDARY])?;
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.inner.flush()
     }
 }
 
@@ -71,12 +91,21 @@ impl<R: Read> Read for Hdlc<R> {
 
 #[cfg(test)]
 mod tests {
-    use std::io::Read;
+    use std::io::{Read, Write};
 
     use crate::hdlc::Hdlc;
 
     #[test]
-    fn preserves_unescaped() {
+    fn write_escape() {
+        let unescaped = vec![0x00, 0x7e, 0x03, 0x7e, 0x7e];
+        let mut buf: Vec<u8> = Vec::new();
+        let mut writer = Hdlc::new(&mut buf);
+        writer.write_all(&unescaped).expect("write");
+        println!(">>> {buf:02x?}");
+    }
+
+    #[test]
+    fn read_preserves_unescaped() {
         let escaped = vec![0x00, 0x01, 0x02, 0x03, 0x04];
         let mut reader = Hdlc::new(escaped.as_slice());
 
@@ -87,7 +116,7 @@ mod tests {
         assert_eq!(buf, escaped);
     }
     #[test]
-    fn unescapes_accross_buffer_boundaries() {
+    fn read_unescapes_accross_buffer_boundaries() {
         let escaped = vec![0x00, 0x7d, 0x5e, 0x03, 0x7d, 0x5e, 0x7d, 0x5e];
         let mut reader = Hdlc::new(escaped.as_slice());
 
@@ -112,7 +141,7 @@ mod tests {
     }
 
     #[test]
-    fn unescapes_all() {
+    fn read_unescapes_all() {
         let escaped = vec![0x7e, 0x00, 0x7d, 0x5e, 0x03, 0x7d, 0x5e, 0x7d, 0x5e, 0x7e];
         let mut reader = Hdlc::new(escaped.as_slice());
 
